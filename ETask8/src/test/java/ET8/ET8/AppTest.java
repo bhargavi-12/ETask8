7fed4bedcd3fa6cdc3fd737c9b2d294d39a8dfca
bhargavi-12 <59757735+bhargavi-12@users.noreply.github.com>
2020-03-31 13:17:21 +0530

    Add files via upload

---
+++ ETask8/src/test/java/ET8/ET8/AppTest.java
@@ -0,0 +1,20 @@
+package ET8.ET8;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
